de020b621f42cad43478b8f8c9b7dc542891f9d4
sara <sara.atef001@gmail.com>
2024-11-19 12:18:49 +0200

    day03

---
+++ JS/Day03/demo.js
@@ -0,0 +1,82 @@
+// var trackName = "frontEnd";
+// function sum( x, y ) {
+//     var trackName="dotnet"
+// 	console.log("x : ", x);
+// 	console.log("y : ", y);
+// 	console.log(trackName);
+// }
+
+// var result = sum(2, 3);
+// console.log(trackName);
+// console.log(result);
+// var trackName = "frontEnd";
+// function sum( x, y ) {
+//      trackName="dotnet"
+// 	console.log("x : ", x);
+// 	console.log("y : ", y);
+// 	console.log(trackName);
+// }
+
+// var result = sum(2, 3);
+// console.log(trackName);
+// console.log(result);
+
+// var trackName = "frontEnd";
+// function sum(x, y) {
+// 	var trackName = "dotnet";
+// 	console.log(trackName);
+// }
+
+// var result = sum(2, 3);
+
+// console.log(result);
+
+// function sum(x, y) {
+// 	var fname = "ali";
+// 	console.log(fname);
+// }
+
+// var result = sum(2, 3);
+// console.log(fname);
+// console.log(result);
+// console.log(square);
+// var square = function (x) {
+// 	var num = 10;
+// 	return x + num;
+// };
+
+// console.log(output);
+// var output = square(6);
+// console.log(output);
+
+// function test() {
+// 	console.log(fname);
+// 	var fname = 10;
+// }
+
+// test();
+
+// function dotNetTrack(supervisorName) {
+// 	console.log("Welcome Dotnet your sup is : ", supervisorName);
+// }
+
+// var greet = function (value) {
+// 	var result = dotNetTrack(value);
+// 	return result;
+// };
+
+// greet("Ahmed Ali");
+
+// var elements = div.getElementsByClassName("info");
+
+// var element = document.querySelector("p.info");
+// console.log(element);
+// for (var i = 0; i < elements.length; i++) {
+// 	console.log(elements[i]);
+// }
+
+// var div = document.getElementById("parent");
+
+// console.log(document.images);
+// console.log(document.links[1]);
+// console.log(document.links[2]);
